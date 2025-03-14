c34cdb0fa8338495d8c72b926f4262f7dc5f4e3f
리코타치즈 <133624013+lico0531@users.noreply.github.com>
2025-03-14 22:49:06 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.static("public")); // 정적 파일(css, js) 제공
 app.get("/led/:state", async (req, res) => {
     const state = req.params.state; // "on" 또는 "off"
     try {
-        //await axios.get(`http://192.168.219.108:8080/led/${state}`); // 라즈베리파이에 요청(로컬ip)
-        await axios.get(`http://115.143.41.38:8080/led/${state}`); // 라즈베리파이 공인ip
+        await axios.get(`http://192.168.219.108:8080/led/${state}`); // 라즈베리파이에 요청(로컬ip)
+        //await axios.get(`http://115.143.41.38:8080/led/${state}`); // 라즈베리파이 공인ip
         res.send(`LED turned ${state}`);
     } catch (error) {
         res.status(500).send("Error controlling LED");
